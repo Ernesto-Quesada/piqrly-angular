a71c93b11228fdd3f866637e277160e08493fd46
ernesqr <ernesqr@yahoo.com>
2026-02-05 03:01:14 -0500

    steipe key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,6 @@ export const environment = {
   apiBaseUrl: 'https://api.piqrly.com',
   webBaseUrl: 'https://piqrly.com',
   cloudfrontBaseUrl: 'https://d1z4r9m5dhahdu.cloudfront.net',
+  stripePublishableKey:
+    'pk_test_51RCXTD4Je26dbkiYQmhsN66oK2Z4LQF2owx1Ybscen0YJnnRV0YuIzCr2HySJHJHE4bGm6BcBXqPuuFIVsnBYLtu00ffGSM70m',
 };
