05da9ca178d8b088c9c4625c44c7fd2cda5da5ad
ernesqr <ernesqr@yahoo.com>
2026-02-05 03:26:42 -0500

    api change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.piqrly.com',
+  apiBaseUrl: 'http://api.piqrly.com',
   webBaseUrl: 'https://piqrly.com',
   cloudfrontBaseUrl: 'https://d1z4r9m5dhahdu.cloudfront.net',
   stripePublishableKey:
